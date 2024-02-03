ce627eced5c17993949e42c79e8f1721316d8c40
Nisha Poudel <npoudel2@uco.edu>
2024-02-03 07:51:23 -0600

    initial

---
+++ public/controller/firebase_core.js
@@ -0,0 +1,18 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA7Gzry59VEi1YroRNLalBsSLjOOFVGjYw",
+    authDomain: "npoudel2-cmsc4373-webapp.firebaseapp.com",
+    projectId: "npoudel2-cmsc4373-webapp",
+    storageBucket: "npoudel2-cmsc4373-webapp.appspot.com",
+    messagingSenderId: "412030683762",
+    appId: "1:412030683762:web:56744c62f51ebd031e90eb"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
